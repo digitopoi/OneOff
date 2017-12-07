a44da7f290e31af5b4dd7a73dc5fce248c2bcb54
digitopoi <hubo85@gmail.com>
2017-12-07 10:01:00 -0600

    Configure registration form

--- OneOff.Web.SPA/src/app/app.module.ts
+++ OneOff.Web.SPA/src/app/app.module.ts
@@ -1,27 +1,36 @@
 //  Angular Modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 //  Material Components
 import {
         MatToolbarModule,
-        MatButtonModule
+        MatButtonModule,
+        MatFormFieldModule,
+        MatInputModule
 } from '@angular/material';
 
 //  Angular Components
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { RegistrationComponent } from './components/registration/registration.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
     MatToolbarModule,
-    MatButtonModule
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
