d920a0210c8155f0f18ec862cf1ea1351c3fa4a7
digitopoi <hubo85@gmail.com>
2017-12-11 17:49:34 -0600

    Configure auth service methods

--- OneOff.Web.SPA/src/app/components/landing/landing.component.ts
+++ OneOff.Web.SPA/src/app/components/landing/landing.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-landing',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _authService: AuthService) { }
 
   ngOnInit() {
+  }
+
+  onLogin() {
+    this._authService.login();
   }
 
 }
